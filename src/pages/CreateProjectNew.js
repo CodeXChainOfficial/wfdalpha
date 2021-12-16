3e07d77983e7824184349d7656cd97ce9e240740
H <94094948+WeFundOfficial@users.noreply.github.com>
2021-12-17 02:48:46 +0700

    Update CreateProjectNew.js

--- src/pages/CreateProjectNew.js
+++ src/pages/CreateProjectNew.js
@@ -48,9 +48,9 @@ export default function NewProject() {
       width:'100%', color:'white', fontSize:'18px', fontFamily:'Sk-Modernist-Regular', fontWeight:'700' }}>
         <div style={{backgroundImage:"url('/createproject_banner_emphasis.svg')", 
         boxShadow:"0px 5px 50px 0px #000000A6", width:'100%', zIndex:'10'}}>
-        <div style={{backgroundImage:"url('/createproject_banner.svg')", width:'100%', zIndex:'11'}}>
+        <div style={{backgroundImage:"url('/createproject_banner.svg')", width:'100%', zIndex:'11',backgroundPosition:'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
           <Flex pt='64px' justify="center">
-            <Text fontSize='16px' fontWeight='400'>Home &gt;&nbsp;</Text>
+            <Text fontSize='16px' fontWeight='400' color={'rgba(255, 255, 255, 0.54)'}>Home &gt;&nbsp;</Text>
             <Text fontSize='16px'>Create Your Project</Text>
           </Flex>
           <Flex mt='11px' pb='75px' mb="75px" justify='center'
@@ -60,8 +60,8 @@ export default function NewProject() {
           </Flex>
         </div>
         </div>
-        <Flex width='100%' justify='center' mt='-80px' px='175px'>
-        <div style={{width:'900px', background:"#FFFFFF0D", fontFamily:'Sk-Modernist-Regular', paddingLeft:'50px', paddingRight:'50px'}} >
+        <Flex width='100%' justify='center' mt='-80px' px='175px' zIndex={'1'}>
+        <div style={{width:'900px', background: 'rgba(255, 255, 255, 0.05)', border: '1.5px solid rgba(255, 255, 255, 0.15)',borderTopColor: 'transparent', fontFamily:'Sk-Modernist-Regular', paddingLeft:'50px', paddingRight:'50px', zIndex:'1'}} >
           {/* --------Select UST or WFD------------------ */}
           <Text fontSize='18px' pt='50px'>Select Back on</Text>
 
@@ -81,7 +81,7 @@ export default function NewProject() {
             >
               <Img mt='23px' boxSize='50px' objectFit='cover' src='/UST.svg' alt='UST Avatar'/>
               <Text mt='13px'>UST</Text>
-              <Img mt='15px' boxSize='20px' objectFit='cover' src={isUST?'/group_dot.svg':'/group_undot.svg'} alt='UST Avatar'/>
+              <Img mt='15px' mb='20px' boxSize='20px' objectFit='cover' src={isUST?'/group_dot.svg':'/group_undot.svg'} alt='UST Avatar'/>
             </ImageTransition>
 
             {/* --------------------------WFD------------------------- */}
@@ -97,15 +97,15 @@ export default function NewProject() {
               width='120px' height='160px' rounded='10px' ml='20px'
               onClick={()=>{setIsUST(false)}}
             >
-              <Img mt='23px' height='50px' objectFit='cover' src='/WeFund Logos only.png' alt='UST Avatar'/>
+              <Img mt='23px' height='35px' objectFit='cover' src='/WeFund Logos only.png' alt='UST Avatar'/>
               <Text mt='13px'>WFD</Text>
-              <Img mt='15px' boxSize='20px' objectFit='cover' src={isUST?'/group_undot.svg':'/group_dot.svg'} alt='UST Avatar'/>  
+              <Img mt='25px' mt="15px" boxSize='20px' objectFit='cover' src={isUST?'/group_undot.svg':'/group_dot.svg'} alt='UST Avatar'/>  
             </ImageTransition>
           </Flex>
           {/* --------project name-------------- */}
           <Box mt='40px'>
             <Flex justify="space-between">
-              <Text>Project Name</Text>
+              <Text mb='20px'>Project Name</Text>
               <Text fontSize='15px'  opacity='0.5'>0/150 words</Text>
             </Flex>
             <InputTransition 
@@ -113,15 +113,15 @@ export default function NewProject() {
               selected={prjName==''?false:true}
               width='100%' height='55px' rounded='md'
             >
-              <InputGroup style={{border:'0', background:'transparent'}} size="sm" border='0px'>
-                <Input style={{border:'0', background:'transparent'}} type="text" h='55px'  rounded="md"  value={prjName} placeholder='Type here'  />
+              <InputGroup style={{background: 'rgba(255, 255, 255, 0.05)', }} size="sm" border='0px'>
+                <Input style={{border:'0', background:'transparent' }} type="text" h='55px'  rounded="md"  value={prjName} placeholder='Type here'  />
               </InputGroup>
             </InputTransition>
           </Box>
           {/* ------------project description--------------- */}
           <Box mt='40px'>
             <Flex justify="space-between">
-              <Text>Project Description</Text>
+              <Text mb='20px'>Project Description</Text>
               <Text fontSize='15px' opacity='0.5'>0/1000 words</Text>
             </Flex>
             <InputTransition 
@@ -129,7 +129,7 @@ export default function NewProject() {
               selected={prjDescription==''?false:true}
               width='100%' height='175px' rounded='md'
             >
-              <Textarea style={{border:'0', background:'transparent'}} value={prjDescription} 
+              <Textarea style={{background: 'rgba(255, 255, 255, 0.05)', }} value={prjDescription} 
               onChange={(e)=>{setPrjDescription(e.target.value)}} rounded="md"
                 placeholder='Type here' size='sm' h='175px' />
             </InputTransition>
@@ -138,16 +138,16 @@ export default function NewProject() {
           <Flex direction='row' mt='40px' justify='space-between'>
             <Box w='100%'>
               <Flex justify="space-between">
-                <Text>Project Website</Text>
+                <Text mb='20px'>Project Website</Text>
               </Flex>
               <InputTransition 
                 unitid='projectwebsite'
                 selected={prjWebsite==''?false:true}
                 width='100%' height='55px' rounded='md'
               >              
-                <InputGroup size="sm" style={{border:'0', background:'transparent'}}>
-                  <InputLeftAddon h='55px' style={{border:'0', background:'transparent'}} children="http://" color='white' rounded="md" />
-                  <Input type="text" h='55px'style={{border:'0', background:'transparent'}} placeholder="Type here" rounded="md"  
+                <InputGroup size="sm" style={{background: 'rgba(255, 255, 255, 0.05)', }}>
+                  <InputLeftAddon h='55px' style={{background: 'rgba(255, 255, 255, 0.05)', }} children="http://" color='white' rounded="md" />
+                  <Input type="text" h='55px'style={{background: 'rgba(255, 255, 255, 0.05)', }} placeholder="Type here" rounded="md"  
                   value={prjWebsite} onChange={(e)=>{setPrjWebsite(e.target.value)}} />
                 </InputGroup>
               </InputTransition>
@@ -155,13 +155,13 @@ export default function NewProject() {
             {/* ---------------upload---------------------------------- */}
             <Box ml='24px' w='100%'>
               <Flex justify="space-between">
-                <Text>Project WhitePaper</Text>
+                <Text mb='20px'>Project WhitePaper</Text>
               </Flex>
               {whitepaper == '' && 
                 <InputGroup size="sm">
                   <InputLeftElement h='55px' pointerEvents='none' children={<IoCloudUploadOutline color='#00A3FF' width='30px' height='30px'/>} />
                   <Input type="text" h='55px' bg='#FFFFFF' borderColor="#FFFFFF33" placeholder="Upload here" focusBorderColor="purple.800"  rounded="md"  
-                  onClick={(e)=>{handleWhitepaper()}} /> 
+                  onClick={(e)=>{handleWhitepaper()}}  /> 
                 </InputGroup>}
               {whitepaper != '' && 
                 <InputGroup size="sm">
@@ -176,16 +176,16 @@ export default function NewProject() {
           {/* --------------project Team description------- */}
           <Box mt='40px'>
             <Flex justify="space-between">
-              <Text>Project Team Description</Text>
+              <Text mb='20px'>Project Team Description</Text>
               <Text  fontSize='15px' opacity='0.5'>0/1000 words</Text>
             </Flex>
             <InputTransition 
               unitid='prjTeamdescription'
               selected={prjTeamdescription==''?false:true}
               width='100%' height='175px' rounded='md'
-              style={{border:'0', background:'transparent'}}
+              style={{background: 'transparent',border:'0'}}
             >
-              <Textarea style={{border:'0', background:'transparent'}} h='165px' value={prjTeamdescription} onChange={(e)=>{setPrjTeamdescription(e.target.value)}}
+              <Textarea style={{background: 'transparent',border:'0'}} h='165px' value={prjTeamdescription} onChange={(e)=>{setPrjTeamdescription(e.target.value)}}
                 placeholder='Type here' size='sm'  rounded="md"
               />
             </InputTransition>
@@ -193,7 +193,7 @@ export default function NewProject() {
           {/* ------------------project category---------- */}
           <Box mt='40px' w='50%'>
             <Flex justify="space-between">
-              <Text>Project Category</Text>
+              <Text mb='20px'>Project Category</Text>
             </Flex>
 
             <InputTransition 
@@ -201,7 +201,7 @@ export default function NewProject() {
               selected={prjCategory==''?false:true}
               width='100%' height='55px' rounded='md'
             >       
-              <Select id="sub_category" style={{border:'0', background:'transparent'}} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
+              <Select id="sub_category" style={{background: 'rgba(255, 255, 255, 0.05)', }} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
                 value='' onChange={(e)=>{setPrjCategory(e.target.value)}} 
               >
                 <option selected style={{backgroundColor:'#1B0645'}}>Crypto</option>
@@ -213,14 +213,14 @@ export default function NewProject() {
           {/* -------------------project sub category--------------- */}
           <Box mt='40px' w='50%'>
             <Flex justify="space-between">
-              <Text>Project Sub Category</Text>
+              <Text mb='20px'>Project Sub Category</Text>
             </Flex>
             <InputTransition 
               unitid='projectsubcategory'
               selected={prjSubcategory==''?false:true}
               width='100%' height='55px' rounded='md'
             >       
-              <Select id="sub_category" style={{border:'0', background:'transparent'}} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
+              <Select id="sub_category" style={{background: 'rgba(255, 255, 255, 0.05)', }} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
                 value='' onChange={(e)=>{setPrjSubcategory(e.target.value)}} 
               >
                 <option selected style={{backgroundColor:'#1B0645'}}>Crypto</option>
@@ -232,14 +232,14 @@ export default function NewProject() {
           {/* ------------------------blockchain category----------------- */}
           <Box mt='40px' w='50%'>
             <Flex justify="space-between">
-              <Text>Blockchain category</Text>
+              <Text mb='20px'>Blockchain category</Text>
             </Flex>
             <InputTransition 
               unitid='projectchain'
               selected={prjChain==''?false:true}
               width='100%' height='55px' rounded='md'
             >       
-              <Select id="sub_category" style={{border:'0', background:'transparent'}} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
+              <Select id="sub_category" style={{background: 'rgba(255, 255, 255, 0.05)', }} h='55px' name="sub_category" autoComplete="sub_category" focusBorderColor="purple.800" shadow="sm" size="sm" w="full" rounded="md"
                 value='' onChange={(e)=>{setPrjChain(e.target.value)}} 
               >
                 <option selected style={{backgroundColor:'#1B0645'}}>Crypto</option>
@@ -252,33 +252,33 @@ export default function NewProject() {
           <Flex direction='row' mt='40px' justify="space-between">
             <Box w='100%'>
               <Flex justify="space-between">
-                <Text>Email</Text>
+                <Text mb='20px'>Email</Text>
               </Flex>
               <InputTransition 
                 unitid='projectemail'
                 selected={prjEmail==''?false:true}
                 width='100%' height='55px' rounded='md'
               >      
-                <InputGroup size="sm" style={{border:'0', background:'transparent'}}>
-                  <InputLeftElement style={{border:'0', background:'transparent'}} pointerEvents='none' color='gray.300' fontSize='1.2em' children=' ' />
-                  <Input style={{border:'0', background:'transparent'}} type="email" h='55px'placeholder="example@email.com" focusBorderColor="purple.800" rounded="md"  value={prjEmail} onChange={(e)=>{setPrjEmail(e.target.value)}} />
+                <InputGroup size="sm" style={{background: 'rgba(255, 255, 255, 0.05)'}}>
+                  <InputLeftElement style={{background: 'transparent', }} pointerEvents='none' color='gray.300' fontSize='1.2em' children=' ' />
+                  <Input style={{ }} type="email" h='55px'placeholder="example@email.com" focusBorderColor="purple.800" rounded="md"  value={prjEmail} onChange={(e)=>{setPrjEmail(e.target.value)}} />
                 </InputGroup>
               </InputTransition>
             </Box>
             <Box ml='24px' w='100%'>
               <Flex justify="space-between">
-                <Text>Amount Required</Text>
+                <Text mb='20px'>Amount Required</Text>
               </Flex>
               <InputTransition 
                 unitid='projectamount'
                 selected={prjEmail==''?false:true}
                 width='100%' height='55px' rounded='md'
               >      
-                <InputGroup size="sm" style={{border:'0', background:'transparent'}}>
-                  <Input style={{border:'0', background:'transparent'}} type="text"  h='55px' placeholder="Type here" focusBorderColor="purple.800" rounded="md"  value={prjAmount} onChange={(e)=>{setPrjAmount(e.target.value)}} />
-                  <InputRightElement style={{border:'0', background:'transparent'}} w='125px'  h='55px' pointerEvents='none' align='center'
+                <InputGroup size="sm" style={{background: 'rgba(255, 255, 255, 0.05' }}>
+                  <Input style={{border:'0', background:'transparent' }} type="text"  h='55px' placeholder="Type here" focusBorderColor="purple.800" rounded="md"  value={prjAmount} onChange={(e)=>{setPrjAmount(e.target.value)}} />
+                  <InputRightElement style={{border:'0', background:'transparent'}} w='125px'  h='55px' pointerEvents='none' align='center' color="blue.200"
                   /> 
-                  <Select id="peg" style={{border:'0', background:'transparent'}} h='55px' w='140px' name="peg" autoComplete="peg" focusBorderColor="purple.800" shadow="sm" size="sm" rounded="md" fontSize='16px' value='' onChange={(e)=>{setPrjChain(e.target.value)}} 
+                  <Select id="peg" style={{border:'0', background:'transparent' }} h='55px' w='140px' name="peg" autoComplete="peg" focusBorderColor="purple.800" shadow="sm" size="sm" rounded="md" fontSize='16px' value='' onChange={(e)=>{setPrjChain(e.target.value)}} 
                   >
                     <option selected style={{backgroundColor:'#1B0645'}}>($)USD</option>
                     <option style={{backgroundColor:'#1B0645'}}>($)USD</option>
